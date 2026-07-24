f1b8e1b59fd97f6e688c3926d1e98d1529b886a5
Carreño Parra <DCARRE6@bancodebogota.com.co>
2026-07-24 17:51:39 -0500

    feat: automate frontend deployment

--- terraform/main.tf
+++ terraform/main.tf
@@ -196,3 +196,7 @@ resource "aws_s3_bucket_policy" "frontend" {
 
   policy = data.aws_iam_policy_document.frontend_bucket_policy.json
 }
+
+output "cloudfront_distribution_id" {
+  value = aws_cloudfront_distribution.frontend.id
+}
